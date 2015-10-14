d989650959df8540cdd868dd2c2772f7e0f5a0bc
Long Le <long.le@dws.la>
2015-10-14 11:01:48 +0200

    update test command, add node to env for zsh

---
+++ README.md
@@ -0,0 +1,20 @@
+# Node Paxos
+
+Node implementation of Paxos
+
+## Build status
+<div style="text-align:center"><img src ="https://codeship.com/projects/YOUR_PROJECT_UUID/status?branch=master" /></div>
+
+## Development
+
+**Requirements:**
+
+1. Vagrant
+
+```sh
+vagrant up
+```
+
+That's all
+
+
--- vagrant/puppet/manifests/puppet.pp
+++ vagrant/puppet/manifests/puppet.pp
@@ -35,8 +35,23 @@ class { 'ohmyzsh': }
 
 ohmyzsh::install { 'vagrant': }
 
+class updateZsh {
+  file { '/home/vagrant/.zshrc':
+    ensure => present,
+  }->
+  file_line { 'Append a line to /home/vagrant/.zshrc':
+    path => '/home/vagrant/.zshrc',
+    line => ' # set env var for node
+NODEJS_HOME=/usr/local/node/node-default
+
+if [ -d "$NODEJS_HOME/bin" ] ; then
+  export PATH="$NODEJS_HOME/bin:$PATH"
+fi',
+  }
+}
 include apt_update
 include othertools
-include nodejs
 include ohmyzsh
+include nodejs
+include updateZsh
 
